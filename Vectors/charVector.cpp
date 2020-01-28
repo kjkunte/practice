7bcc5fbfb25443f73d28bb72d564f4eeec62209d
kj <kj@ubuntu.ubuntu-domain>
2020-01-28 12:16:08 +0530

    some modifications

---
+++ Vectors/charVector.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+
+  vector<char> array;
+  char c = 0;
+  while(c!='x') {
+    cin>>c;
+    array.push_back(c);
+  }
+
+}
