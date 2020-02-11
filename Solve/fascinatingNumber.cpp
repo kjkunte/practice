c9e9a014b8e4593aad577628b54162163b426f41
kj <kj@ubuntu.ubuntu-domain>
2020-02-11 15:37:19 +0530

    fascinating number gone wrong

--- Solve/fascinatingNumber.cpp
+++ Solve/fascinatingNumber.cpp
@@ -1,34 +1,55 @@
 #include<iostream>
 #include<string>
 using namespace std;
-
-int main() {
-    
-    int x1;
-    cin>>x1;
-    findFascinatingNumber(x1);
-    return 0;
-}
-
 // for concatenating two numbers, they have to be converted to string
 int findFascinatingNumber(int x1){
     int x2 = x1*2;
-    string s1 = to_string(x2);
     int x3 = x1*3;
-    string s2 = to_string(x3);
+    cout<<x2;
+    // string s1 = to_string(x2);
+    // cout<<"s1 is"<<s1<<endl;
+    string s1,s2;
+    s1.push_back((char)(x2 + '0'));
+    s2.push_back((char)(x3 + '0'));
+    
+    // string s2 = to_string(x3);
+    cout<<"s2 is"<<s2<<endl;
     string s3 = s1 + s2;
+    cout<<"s3 = "<<s3;
     int * arr = new int [s3.length()-1]; // Declaring new dynamic array with size 1 less than that of the string
     for(int i = 0; i<s3.length(); i++){
             arr[i]=s3[i];
     }  
+    // cout<<*arr<<endl;
     //sorting the array
     for(int i = 0; i<s3.length(); i++){
         if(arr[i]>arr[i+1]){
             arr[i]=arr[i+1];
+            // cout<<arr[i];
         }
     }
-    
-    // int c = stoi(s3);
 
+    for(int i = 0; i<s3.length(); i++){
+        if(arr[i]==arr[i+1]){
+          return 0;
+        }
+        else {return 1;}
+    }
+    delete arr;
+    // int c = stoi(s3);
 }
+
+int main() {
+    
+    int x1,t=0;
+    cin>>t;
+    
+    while(t--!=0){
+    cin>>x1;
+    cout<<findFascinatingNumber(x1)<<endl;
+    }  
+    return 0;
+}
+
+
 
---
+++ VeryBasics/XOR.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int arr1 [5] = {1,1,3,4,5};
+    for(int i; i<=4; i++){
+        arr1[i]^arr1[i++];
+    }
+    int arr2 [5] = {1,2,3,4,5};
+
+}
