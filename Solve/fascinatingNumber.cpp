40dfa5ce04374f4abecb00b520274bee49c4b136
kj <kj@ubuntu.ubuntu-domain>
2020-02-17 10:15:50 +0530

    duplicate in an array

--- Solve/fascinatingNumber.cpp
+++ Solve/fascinatingNumber.cpp
@@ -5,36 +5,37 @@ using namespace std;
 int findFascinatingNumber(int x1){
     int x2 = x1*2;
     int x3 = x1*3;
-    cout<<x2;
-    // string s1 = to_string(x2);
-    // cout<<"s1 is"<<s1<<endl;
-    string s1,s2;
-    s1.push_back((char)(x2 + '0'));
-    s2.push_back((char)(x3 + '0'));
-    
-    // string s2 = to_string(x3);
-    cout<<"s2 is"<<s2<<endl;
-    string s3 = s1 + s2;
-    cout<<"s3 = "<<s3;
-    int * arr = new int [s3.length()-1]; // Declaring new dynamic array with size 1 less than that of the string
-    for(int i = 0; i<s3.length(); i++){
-            arr[i]=s3[i];
-    }  
-    // cout<<*arr<<endl;
-    //sorting the array
-    for(int i = 0; i<s3.length(); i++){
-        if(arr[i]>arr[i+1]){
-            arr[i]=arr[i+1];
-            // cout<<arr[i];
-        }
+    string s2 = to_string(x2);
+    string s3 = to_string(x3);
+    string s4 = s2 + s3;
+    // string s4 = "Kshitij";
+    // cout<<s4.length()<<endl;
+
+    int * arr = new int [s4.length()-1]; // Declaring new dynamic array with size 1 less than that of the string
+    for(int p=0; p<s4.length(); p++){
+        // when we are converting the string of integers to an integer array the ascii value of the integers gets stored
+          arr[p]=static_cast<char>(s4[p]);
+            // cout<<arr[p]<<endl;
     }
 
-    for(int i = 0; i<s3.length(); i++){
-        if(arr[i]==arr[i+1]){
-          return 0;
+    // sorting the array
+    for(int i = 0; i<s4.length(); i++){
+        for(int j = i+1; j<s4.length(); j++ ){
+            if(arr[i]>arr[j]){
+                int temp = arr[i+1];
+                arr[j]=arr[i];
+                arr[i] = temp;
+            }
         }
-        else {return 1;}
+        cout<<arr[i]<<endl;
     }
+
+    // for(int i = 0; i<s4.length(); i++){
+    //     if(arr[i]==arr[i+1]){
+    //       return 0;
+    //     }
+    //     else {return 1;}
+    // }
     delete arr;
     // int c = stoi(s3);
 }
