3fa966ebf373272692b264a09593ab267e8ce5a9
kj <kj@ubuntu.ubuntu-domain>
2020-02-29 12:58:15 +0530

    delete element from an multidimensional array

--- Solve/arrayOperation.js
+++ Solve/arrayOperation.js
@@ -5,23 +5,25 @@ function filteredArray(arr, elem) {
       for (let i = 0; i<arr.length; i++){
           console.log('1st for' + i);
         for (let j = 0; j<arr[i].length;j++){
-            console.log('2nd for')
-          if(arr[i][j] == elem)
+            console.log('2nd for' + arr[i])
+          if(arr[i][j] === elem)
           {
-            console.log('delete')
-            arr.splice(arr[i],1)
-            i--
+            console.log('delete' + arr[i])
+            // console
+            arr.splice(i,1)
+            i-- // This addition operation can be avoided if we travese the intial for loop in reverse order
+            // Baaammm!!!!! I can write the logic
             break
           }
         }
       }
-    //   newArr = [...arr]
-    for(let k = 0; k<arr.length; k++){
-        console.log(arr[k]);
-    }
+      newArr = [...arr]
+    // for(let k = 0; k<arr.length; k++){
+    //     console.log(arr[k]);
+    // }
     // change code above this line
     return newArr;
   }
   
   // change code here to test different cases:
-  console.log(filteredArray([[3, 2, 3], [1, 6, 0], [3, 13, 26], [19, 3, 9]], 3));
+  console.log(filteredArray([ ["amy", "beth", "sam"], ["dave", "sean", "peter"] ], "peter"));
