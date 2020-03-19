c037e9a79fe16c949d06bd2d199219a92b81a898
kj <kj@ubuntu.ubuntu-domain>
2020-03-19 11:10:52 +0530

    longest word in a string

--- Solve/longestWord.js
+++ Solve/longestWord.js
@@ -1,17 +1,26 @@
 function findLongestWordLength(str) {
   var str1 = '';
-  str1 = str.split('')
-  count = 0;
-  spaceIndex = 0
- console.log(str1.length)
-  for(var i = str1.length - 1; i>=0; i--){
+  str1 = str.split('');
+//  console.log(str1.length)
+  var arr = []
+  arr.push(str.length)
+  var diff = 0 
+  for(var i = str1.length-1; i>=0; i--){
     if(str1[i] === ' '){
-      diff = i - count;
-      count = i;
-      console.log(diff)
+      arr.push(i);       
     }
   }
-  console.log(str1)
+  arr.push(-1)
+  for(var j = 0; j<=arr.length; j++){
+    // diff = arr[j]-arr[j+1]
+    if(diff<arr[j]-arr[j+1]){
+      diff = arr[j]-arr[j+1]
+      // console.log(diff)
+    }
+  }
+  // console.log(arr);
+  console.log(diff -1);
+  return (diff -1);
 }
 
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
